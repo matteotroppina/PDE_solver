951136a1d6e766650272242e6dd81455161e021c
matteot <matteo.troppina@yahoo.it>
2023-11-28 20:23:03 +0100

    small changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <memory>
 #include <vector>
 
-// Node - coordinates, temperature
 struct Node {
   size_t x, y;
   double temperature;
@@ -11,7 +10,6 @@ struct Node {
 
 using meshType = std::vector<std::vector<Node>>;
 
-// Mesh
 class Mesh {
 private:
   std::unique_ptr<meshType> nodes; // pointer to a 2D vector of nodes
@@ -21,10 +19,27 @@ public:
   Mesh(const size_t width, const size_t height) {
     nodes = std::make_unique<meshType>(height, std::vector<Node>(width));
   }
+
+  // method .getNode(x,y)
+  // return the a pointer to the (x,y) node
+  Node Mesh::getNode(const size_t x, const size_t y) {
+    return (*nodes).at(x).at(y);
+  };
 };
 
 int main() {
 
-  size_t width, height;
+  // mesh initialization
+  size_t height; // rows
+  size_t width;  // columns
+
+  std::cout << "** Mesh Initialization ** \n";
+  std::cout << "Mesh height (rows):" << std::endl;
+  std::cin >> height;
+  std::cout << "Mesh width (columns):" << std::endl;
+  std::cin >> width;
+
+  Mesh myMesh(width, height);
+
   return 0;
 }
